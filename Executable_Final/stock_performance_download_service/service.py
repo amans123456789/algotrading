feb2e71a72878f83c6879f01ecf26419b5945084
unknown <amansehgal003@gmail.com>
2023-11-07 23:21:04 +0530

    commented service.py __main__ code

--- Executable_Final/stock_performance_download_service/service.py
+++ Executable_Final/stock_performance_download_service/service.py
@@ -3,11 +3,10 @@ from stock_performance import stock_data_save
 
 app = FastAPI()
 
-# @app.get("/process_stock_data/")
 @app.get("/process_stock_data")
 async def stock_performance_service():
     return stock_data_save.create_csv()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
