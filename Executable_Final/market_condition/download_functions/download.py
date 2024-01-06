716653986ec23753b72219a4b56c6d97be4663c3
unknown <amansehgal003@gmail.com>
2024-01-06 23:09:31 +0530

    market service cleanup

--- Executable_Final/market_condition/download_functions/download.py
+++ Executable_Final/market_condition/download_functions/download.py
@@ -1,18 +1,62 @@
 from download_functions.dwnld_functions import market_data, VIX
 import pandas as pd
 # from config import config
+import logging
+logging.basicConfig(level=logging.INFO)
+import os
 
-def daily_data(mkt_symb,start_time, end_time):
 
-    res = market_data(mkt_symb,"daily",start_time, end_time)
-    vx = VIX("daily",start_time, end_time)
-    fin = pd.merge(vx, res, on='Date')
-    return fin, res, vx
+def daily_data(mkt_symb,start_date, end_date):
+    try:
+        folder_name = (end_date).strftime('%Y-%m-%d')
+        base_folder = 'market_data/final_download'
 
-def minute_data(mkt_symb,start_time, end_time):
+        new_folder_path = os.path.join(base_folder, folder_name)
+        os.makedirs(new_folder_path, exist_ok=True)
 
-    res = market_data(mkt_symb,"minute",start_time, end_time)
-    vx = VIX("minute",start_time, end_time)
-    fin = pd.merge(vx, res, on=['Date','Time'])
-    return fin, res, vx
+        daily_filename = os.path.join(new_folder_path, "fin_daily{}.csv".format(folder_name))
 
+        check = all(os.path.exists(filename) for filename in [daily_filename])
+        if not check:
+
+            res = market_data(mkt_symb,"daily",start_date, end_date)
+            vx = VIX("daily",start_date, end_date)
+            fin = pd.merge(vx, res, on='Date')
+            fin.to_csv(daily_filename)
+
+            return fin
+        else:
+            logging.info(f"CSV files for the mentioned date already exist. Skipping data fetching and processing.")
+            fin = pd.read_csv(daily_filename)
+
+            return fin
+    except Exception as e:
+        logging.error(f"An error occurred in download_func: {str(e)}", exc_info=True)
+
+
+def minute_data(mkt_symb,start_date, end_date):
+    try:
+        folder_name = (end_date).strftime('%Y-%m-%d')
+        base_folder = 'market_data/final_download'
+
+        new_folder_path = os.path.join(base_folder, folder_name)
+        os.makedirs(new_folder_path, exist_ok=True)
+
+        minute_filename = os.path.join(new_folder_path, "fin_minute{}.csv".format(folder_name))
+
+        check = all(os.path.exists(filename) for filename in [minute_filename])
+        if not check:
+
+            res = market_data(mkt_symb,"minute",start_date, end_date)
+            vx = VIX("minute",start_date, end_date)
+            fin = pd.merge(vx, res, on=['Date','Time'])
+            fin.to_csv(minute_filename)
+
+            return fin
+
+        else:
+            logging.info(f"CSV files for the mentioned date already exist. Skipping data fetching and processing.")
+            fin = pd.read_csv(minute_filename)
+            return fin
+    except Exception as e:
+        logging.error(f"An error occurred in download_func: {str(e)}", exc_info=True)
--- Executable_Final/market_condition/service.py
+++ Executable_Final/market_condition/service.py
@@ -1,8 +1,29 @@
 from config import config
+from fastapi import FastAPI, Body
 from download_functions.download import daily_data, minute_data
+from datetime import date, datetime
+import logging
 
-fin_min, res_min, vx_min = (minute_data(config["tickers_NS"] ,"2023-12-21", "2023-12-24"))
-fin_day, res_day, vx_day = (daily_data(config["tickers_NS"],"2023-12-21", "2023-12-24"))
+logging.basicConfig(level=logging.INFO)
 
-fin_min.to_csv("market_data/fin_min.csv")
-fin_day.to_csv("market_data/fin_day.csv")
+app = FastAPI()
+from typing import Union
+from typing_extensions import Annotated
+
+@app.post("/download_market_data")
+async def stock_download_service(
+        start_date: Annotated[Union[date, None], Body()] = None,
+        end_date: Annotated[Union[date, None], Body()] = None
+):
+    logging.info(f"Received end_date: {end_date}")
+    logging.info(f"Received end_date: {start_date}")
+
+    (minute_data(config["tickers_NS"], start_date, end_date))
+    (daily_data(config["tickers_NS"], start_date, end_date))
+
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok", "message": "Health check passed."}
+
+
